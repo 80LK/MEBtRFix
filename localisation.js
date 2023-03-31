60fab78d5b8eb8bf46d0231e70006768015acdc1
80LK <80lkru@gmail.com>
2023-03-31 11:04:05 +0300

    fix name Krogan and Vorcha

---
+++ localisation.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const path = './localisation/'
+const root_language = 'english';
+const root_language_path = path + root_language + '/';
+const langs = fs.readdirSync(path).filter(folder => folder != root_language);
+
+// fs.readdirSync(path + root_language).forEach(file => {
+// 	if (fs.statSync(root_language_path + file).isDirectory()) {
+// 		console.log(`"${file}" is Dir`);
+// 	} else {
+// 		console.log(`"${file}" is File`);
+// 	}
+// });
+
+moveLocales(root_language_path, root_language, langs);
+
+function moveLocales(r_path, lang, langs) {
+	fs.readdirSync(r_path).forEach(file => {
+		const path = r_path + file;
+		if (fs.statSync(path).isDirectory()) {
+			console.log(`"${file}" is Dir`);
+			langs.forEach(value => {
+				const newdir = path.replaceAll(lang, value);
+				if (!fs.existsSync(newdir)) fs.mkdirSync(newdir);
+			})
+			moveLocales(path + "/", lang, langs);
+		} else {
+			langs.forEach(value => {
+				fs.writeFileSync(path.replaceAll(lang, value), fs.readFileSync(path).toString().replace('l_' + lang, 'l_' + value));
+			})
+		}
+	});
+}
