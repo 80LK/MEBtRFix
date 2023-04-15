845485705d299252e1ff920a8814677879fba071
80LK <80lkru@gmail.com>
2023-04-15 03:48:47 +0300

    clear script

--- localisation.js
+++ localisation.js
@@ -5,14 +5,6 @@ const root_language = 'english';
 const root_language_path = path + root_language + '/';
 const langs = fs.readdirSync(path).filter(folder => folder != root_language);
 
-// fs.readdirSync(path + root_language).forEach(file => {
-// 	if (fs.statSync(root_language_path + file).isDirectory()) {
-// 		console.log(`"${file}" is Dir`);
-// 	} else {
-// 		console.log(`"${file}" is File`);
-// 	}
-// });
-
 moveLocales(root_language_path, root_language, langs);
 
 function moveLocales(r_path, lang, langs) {
@@ -20,7 +12,6 @@ function moveLocales(r_path, lang, langs) {
 		if (file == "l_english.yml") return;
 		const path = r_path + file;
 		if (fs.statSync(path).isDirectory()) {
-			console.log(`"${file}" is Dir`);
 			langs.forEach(value => {
 				const newdir = path.replaceAll(lang, value);
 				if (!fs.existsSync(newdir)) fs.mkdirSync(newdir);
